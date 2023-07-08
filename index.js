43a25e728e2a8075ff9690eefee3090cb4b17c62
SimplyKyle! <68589851+JustSimplyKyle@users.noreply.github.com>
2023-07-08 09:31:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-window.open('https://forms.gle/WPSjE2vxGNYH4Lei8', '_blank').focus();
 window.location.href = urls.length > 0 ? urls[Math.floor(Math.random() * urls.length)] : 'https://github.com/hms5232/random-survey';
